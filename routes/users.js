73d656794e401fd816f14045b1df1d55a4652990
Momin Chaudhry <momin@chaudhry.tv>
2021-04-29 16:47:17 -0400

    changed user schema, added admin propety

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.post('/register', async (req, res) => {
 
     try {
         const newUser = await user.save()
-        res.status(201).json({message: 'Successfully registered', username})
+        res.status(201).json({message: 'Successfully registered', user:newUser})
     } catch (err) {
         res.status(400).json({ message: err.message })
     }
