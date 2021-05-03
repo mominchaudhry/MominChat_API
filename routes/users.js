4f4ae27e97c9f7129d8f368808b35a9dac0932cd
Momin Chaudhry <momin@chaudhry.tv>
2021-05-02 23:36:57 -0400

    changed add friend endpoint to add by unique username instead of id

--- routes/users.js
+++ routes/users.js
@@ -178,7 +178,6 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (!token) return res.sendStatus(401)
-    console.log(user)
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403)
